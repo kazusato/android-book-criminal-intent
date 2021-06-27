ddfb8e163d9a45acc4a0319f6d7465ccb06b2682
Kazuhiko Sato <super80@gmail.com>
2021-06-27 10:40:30 +0900

    added fragment

--- app/src/main/java/dev/kazusato/android/criminalintent/MainActivity.kt
+++ app/src/main/java/dev/kazusato/android/criminalintent/MainActivity.kt
@@ -2,10 +2,20 @@ package dev.kazusato.android.criminalintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.kazusato.android.criminalintent.fragment.CrimeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
